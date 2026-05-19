12fcb323967297f8ccae3c62c46c50c34a86a23e
Pranav4005 <pranav.ganimisetti@gmail.com>
2026-05-19 15:55:16 +0530

    setup added

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+REPO_NAME="Book-recommender-system"
+AUTHOR_USER_NAME="Pranav4005"
+LIST_OF_REQUIREMENTS=[]
+
+setup(
+    name='BookRecommenderSystem',
+    version='0.0.0',
+    author='Pranav4005',
+    description="A book recommender system package",
+    packages=find_packages(),
+    url="https://github.com/Pranav4005/Book-recommender-system",
+    install_requires=LIST_OF_REQUIREMENTS
+)
