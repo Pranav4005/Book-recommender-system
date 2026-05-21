0773efb6f3e1bc9cfde435d8aea47a90271c25dc
Pranav4005 <pranav.ganimisetti@gmail.com>
2026-05-21 13:07:18 +0530

    Data Validation added

--- book_recommendation_system/pipeline/training_pipeline.py
+++ book_recommendation_system/pipeline/training_pipeline.py
@@ -1,9 +1,14 @@
 from book_recommendation_system.components.stage_00_data_ingestion import DataIngestion
+from book_recommendation_system.components.stage_01_data_validation import DataValidation
 
 class TrainingPipeline:
     def __init__(self):
         self.data_ingestion = DataIngestion()
+        self.data_validation = DataValidation()
 
     def start_data_ingestion(self):
         self.data_ingestion.initiate_data_ingestion()
-        
+
+
+    def start_data_validation(self):
+        self.data_validation.initiate_data_validation()
--- main.py
+++ main.py
@@ -3,3 +3,4 @@ from book_recommendation_system.pipeline.training_pipeline import TrainingPipeli
 
 obj = TrainingPipeline()
 obj.start_data_ingestion()
+obj.start_data_validation()
