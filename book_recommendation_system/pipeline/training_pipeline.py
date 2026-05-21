1f6bec0b70dde7cdfcc3b1ae3a542eaf174c6a6f
Pranav4005 <pranav.ganimisetti@gmail.com>
2026-05-21 19:18:22 +0530

    Performed data transformation

--- book_recommendation_system/pipeline/training_pipeline.py
+++ book_recommendation_system/pipeline/training_pipeline.py
@@ -1,10 +1,12 @@
 from book_recommendation_system.components.stage_00_data_ingestion import DataIngestion
 from book_recommendation_system.components.stage_01_data_validation import DataValidation
+from book_recommendation_system.components.stage_02_data_transformation import DataTransformation
 
 class TrainingPipeline:
     def __init__(self):
         self.data_ingestion = DataIngestion()
         self.data_validation = DataValidation()
+        self.data_transformation = DataTransformation()
 
     def start_data_ingestion(self):
         self.data_ingestion.initiate_data_ingestion()
@@ -12,3 +14,6 @@ class TrainingPipeline:
 
     def start_data_validation(self):
         self.data_validation.initiate_data_validation()
+
+    def start_data_transformation(self):
+        self.data_transformation.initiate_data_transformation()
--- main.py
+++ main.py
@@ -4,3 +4,4 @@ from book_recommendation_system.pipeline.training_pipeline import TrainingPipeli
 obj = TrainingPipeline()
 obj.start_data_ingestion()
 obj.start_data_validation()
+obj.start_data_transformation()
