bccf31004aba1696ac64708b0c05f000231afee2
Pranav4005 <pranav.ganimisetti@gmail.com>
2026-05-21 12:28:59 +0530

    Added data ingestion

---
+++ book_recommendation_system/pipeline/training_pipeline.py
@@ -0,0 +1,9 @@
+from book_recommendation_system.components.stage_00_data_ingestion import DataIngestion
+
+class TrainingPipeline:
+    def __init__(self):
+        self.data_ingestion = DataIngestion()
+
+    def start_data_ingestion(self):
+        self.data_ingestion.initiate_data_ingestion()
+        
---
+++ main.py
@@ -0,0 +1,5 @@
+from book_recommendation_system.pipeline.training_pipeline import TrainingPipeline
+
+
+obj = TrainingPipeline()
+obj.start_data_ingestion()
